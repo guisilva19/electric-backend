19c6f9345bd6cfddce10d75d3bdb54e66f503de6
unknown <2003silvagui@gmail.com>
2024-09-20 10:39:56 -0300

    feat: ajust

--- src/homologation/homologation.dto.ts
+++ src/homologation/homologation.dto.ts
@@ -1,162 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class HomologationDTO {
   @ApiProperty({
     example: 'string',
   })
-  @IsString()
-  @IsNotEmpty()
   nome: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  @IsNotEmpty()
-  telefone: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  @IsNotEmpty()
-  email: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  numero_conta_contrato: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  distancia_entre_inversor_e_distribuicao: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  tipo_de_ligacao: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  tensao_de_fornecimento: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  disjuntor_do_padrao: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  cabo_do_padrao: string;
-
-  @ApiProperty({
-    example: false,
-  })
-  @IsBoolean()
-  ampliacao: boolean;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  carga_instalada: string;
-
-  @ApiProperty({
-    example: 1,
-  })
-  total_de_inversores: number;
-
-  @ApiProperty({
-    example: 1,
-  })
-  total_de_modulos: number;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  modelo_do_inversor_inserido: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  modelo_do_modulo_inserido: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  quantidade_modulos_inseridos: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  quantidade_inversores_inseridos: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  modelo_do_inversor_homologado: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  modelo_do_modulo_homologado: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  quantidade_modulos_homologados: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  quantidade_inversores_homologados: string;
-
-  ////
-
-  @ApiProperty({
-    example: false,
-  })
-  outras_conta_recebera_credito: boolean;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  numero_conta_contrato_02: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  media_consumo_conta_02: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  numero_conta_contrato_03: string;
-
-  @ApiProperty({
-    example: 'string',
-  })
-  @IsString()
-  media_consumo_conta_03: string;
 }
+
+// "nome":"Guilherme Silva Fernandes",
+// "cabo_do_padrao":"10",
+// "disjuntor_do_padrao":"63",
+// "distancia_entre_inversor_e_distribuicao":"12",
+// "email":"2003silvagui@gmail.com",
+// "modelo_do_inversor_inserido":"1",
+// "modelo_do_modulo_inserido":"1",
+// "numero_conta_contrato":"12345678",
+// "quantidade_inversores_inseridos":"1",
+// "quantidade_medidores":"1",
+// "quantidade_modulos_inseridos":"1",
+// "telefone":"7799664132",
+// "tensao_de_fornecimento":"127/220",
+// "tipo_de_ligacao":"MONOFÁSICA",
+// "transformador":false,
+// "ampliacao":false,
