237fcfef05d9188a80f3509caf3200abd7c6642c
unknown <2003silvagui@gmail.com>
2024-09-28 18:03:20 -0300

    feat: files

--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -3,13 +3,12 @@ import {
   Get,
   Param,
   Post,
-  UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { DocumentsService } from './documents.service';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
-import { MultipartDTO } from './documents.dto';
 
 @ApiTags('Documents')
 @Controller('documents')
@@ -19,15 +18,16 @@ export class DocumentsController {
   @Post(':id')
   @ApiConsumes('multipart/form-data')
   @ApiBody({
-    description: 'Upload a file',
-    type: MultipartDTO,
+    description: 'Upload multiple files',
+    type: 'multipart/form-data',
+    isArray: true,
   })
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FilesInterceptor('files'))
   async upload(
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFiles() files: Express.Multer.File[],
     @Param('id') id: string,
   ) {
-    return this.documentsService.create(file, id);
+    return this.documentsService.create(files, id);
   }
 
   @Get(':id')
--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -20,25 +20,31 @@ export class DocumentsService {
     });
   }
 
-  async create(file: Express.Multer.File, id: string) {
-    const params = {
-      Bucket: process.env.S3_BUCKET_NAME,
-      Key: file.originalname,
-      Body: file.buffer,
-    };
+  async create(files: Express.Multer.File[], id: string) {
+    const uploadedDocuments = [];
 
-    const command = new PutObjectCommand(params);
-    await this.s3.send(command);
+    for (const file of files) {
+      const params = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: file.originalname,
+        Body: file.buffer,
+      };
 
-    const document = this.db.documento.create({
-      data: {
-        nome: file.originalname,
-        url: `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.S3_REGION}.amazonaws.com/${file.originalname}`,
-        homologationId: id,
-      },
-    });
+      const command = new PutObjectCommand(params);
+      await this.s3.send(command);
 
-    return document;
+      const document = await this.db.documento.create({
+        data: {
+          nome: file.originalname,
+          url: `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.S3_REGION}.amazonaws.com/${file.originalname}`,
+          homologationId: id,
+        },
+      });
+
+      uploadedDocuments.push(document);
+    }
+
+    return uploadedDocuments;
   }
 
   async findMyDocs(id: string) {
