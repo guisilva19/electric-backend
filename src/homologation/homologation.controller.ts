a64ad26951e729784376f0685ee37b5a3c33be22
unknown <2003silvagui@gmail.com>
2024-10-03 11:34:47 -0300

    feat: new routes get homologations

--- src/homologation/homologation.controller.ts
+++ src/homologation/homologation.controller.ts
@@ -1,7 +1,16 @@
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { HomologationService } from './homologation.service';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { HomologationDTO } from './homologation.dto';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
 
 @ApiTags('Homologação')
 @Controller('homologation')
@@ -13,13 +22,35 @@ export class HomologationController {
     return await this.homologationService.createHomologation(homologation);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @Get(':id')
-  async listHomologation(@Param('id') id: string) {
-    return await this.homologationService.list(id);
+  async listHomologation(@Req() request, @Param('id') id: string) {
+    const token = request.headers.authorization.split(' ')[1];
+    return await this.homologationService.list(id, token);
   }
 
-  @Get()
-  async listAllHomologations() {
-    return await this.homologationService.listAll();
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @Get('')
+  async listAll(@Req() request) {
+    const token = request.headers.authorization.split(' ')[1];
+    return await this.homologationService.listAll(token);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @Get('/paid')
+  async listAllPaid(@Req() request) {
+    const token = request.headers.authorization.split(' ')[1];
+    return await this.homologationService.listAllPaid(token);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @Get('/nopaid')
+  async listAllNoPaid(@Req() request) {
+    const token = request.headers.authorization.split(' ')[1];
+    return await this.homologationService.listAllNoPaid(token);
   }
 }
--- src/homologation/homologation.service.ts
+++ src/homologation/homologation.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConnectionService } from 'src/connection/connection.service';
 import { HomologationDTO } from './homologation.dto';
 import { PaymentsService } from 'src/payment/payment.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class HomologationService {
   constructor(
     private readonly db: ConnectionService,
     private readonly paymentsService: PaymentsService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async createHomologation(homologation: HomologationDTO) {
@@ -38,7 +40,12 @@ export class HomologationService {
     });
   }
 
-  async list(id: string) {
+  async list(id: string, token: string) {
+    const decoded = this.jwtService.decode(token);
+    if (!decoded || typeof decoded !== 'object' || !decoded.id) {
+      throw new UnauthorizedException('Token inválido ou ausente');
+    }
+
     return this.db.homologation.findUnique({
       where: {
         id: id,
@@ -77,8 +84,101 @@ export class HomologationService {
     });
   }
 
-  async listAll() {
+  async listAll(token: string) {
+    const decoded = this.jwtService.decode(token);
+    if (!decoded || typeof decoded !== 'object' || !decoded.id) {
+      throw new UnauthorizedException('Token inválido ou ausente');
+    }
+
     return this.db.homologation.findMany({
+      select: {
+        documentos: true,
+        ampliacao: true,
+        cabo_do_padrao: true,
+        carga_instalada: true,
+        disjuntor_do_padrao: true,
+        email: true,
+        contas_receber_credito: true,
+        distancia_entre_inversor_e_distribuicao: true,
+        link_payment: true,
+        modelo_do_inversor_homologado: true,
+        modelo_do_inversor_inserido: true,
+        modelo_do_modulo_homologado: true,
+        nome: true,
+        modelo_do_modulo_inserido: true,
+        numero_conta_contrato: true,
+        outras_conta_recebera_credito: true,
+        quantidade_inversores_homologados: true,
+        quantidade_inversores_inseridos: true,
+        quantidade_medidores: true,
+        quantidade_modulos_homologados: true,
+        quantidade_modulos_inseridos: true,
+        status_payment: true,
+        telefone: true,
+        tensao_de_fornecimento: true,
+        tipo_de_ligacao: true,
+        total_de_inversores: true,
+        total_de_modulos: true,
+        transformador: true,
+        id: true,
+      },
+    });
+  }
+
+  async listAllPaid(token: string) {
+    const decoded = this.jwtService.decode(token);
+    if (!decoded || typeof decoded !== 'object' || !decoded.id) {
+      throw new UnauthorizedException('Token inválido ou ausente');
+    }
+
+    return this.db.homologation.findMany({
+      where: {
+        status_payment: true,
+      },
+      select: {
+        documentos: true,
+        ampliacao: true,
+        cabo_do_padrao: true,
+        carga_instalada: true,
+        disjuntor_do_padrao: true,
+        email: true,
+        contas_receber_credito: true,
+        distancia_entre_inversor_e_distribuicao: true,
+        link_payment: true,
+        modelo_do_inversor_homologado: true,
+        modelo_do_inversor_inserido: true,
+        modelo_do_modulo_homologado: true,
+        nome: true,
+        modelo_do_modulo_inserido: true,
+        numero_conta_contrato: true,
+        outras_conta_recebera_credito: true,
+        quantidade_inversores_homologados: true,
+        quantidade_inversores_inseridos: true,
+        quantidade_medidores: true,
+        quantidade_modulos_homologados: true,
+        quantidade_modulos_inseridos: true,
+        status_payment: true,
+        telefone: true,
+        tensao_de_fornecimento: true,
+        tipo_de_ligacao: true,
+        total_de_inversores: true,
+        total_de_modulos: true,
+        transformador: true,
+        id: true,
+      },
+    });
+  }
+
+  async listAllNoPaid(token: string) {
+    const decoded = this.jwtService.decode(token);
+    if (!decoded || typeof decoded !== 'object' || !decoded.id) {
+      throw new UnauthorizedException('Token inválido ou ausente');
+    }
+
+    return this.db.homologation.findMany({
+      where: {
+        status_payment: true,
+      },
       select: {
         documentos: true,
         ampliacao: true,
